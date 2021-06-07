af501e43b8231d0a14d234ef364d74671a68548e
zenetix <mnogolikiy.ilay@gmail.com>
2021-06-07 16:00:28 +0300

    lesson 3 - topic 4

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import './index.css';
 import ExampleRendererLesson3Topic1 from "./Lesson3_1/1_globalEvents";
 import ExampleRendererLesson3Topic2 from "./Lesson3_1/2_sendingRequests";
 import ExampleRendererLesson3Topic3 from "./Lesson3_1/3_editForm";
+import ExampleRendererLesson3Topic4 from "./Lesson3_1/4_refs";
 
-ReactDOM.render(<ExampleRendererLesson3Topic3 />, document.getElementById('root'));
+ReactDOM.render(<ExampleRendererLesson3Topic4 />, document.getElementById('root'));
